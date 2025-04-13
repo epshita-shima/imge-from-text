8e12f150c7c6729f855c17d2c02ad74f89c2605b
epshita-shima <epshitashima@gmail.com>
2025-04-13 12:46:17 +0600

    change cors

--- index.js
+++ index.js
@@ -33,10 +33,17 @@
 // });
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // or specify your frontend domain
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Only POST allowed' });
   }
-
   const { image } = req.body;
 
   if (!image) {
